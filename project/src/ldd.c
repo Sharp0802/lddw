2357def639f22249dc83a450345a81f30ca8f11b
Sharp0802 <sharp0802.business@gmail.com>
2023-02-22 06:09:40 +0900

    Fix bugs & Refactor codes
    - project/src/ldd.c: Re-declare functions to use unicode functions and
    Prefer to use heap allocation rather than using stack allocation to pre-
    vent stack-overflow in recursive functions and Fix a bug that `GetModul-
    eFileName` fails

--- project/src/ldd.c
+++ project/src/ldd.c
@@ -1,60 +1,69 @@
 #include "ldd.h"
 
-#define OpenFileR(f) CreateFile(f, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)
+#define OpenFileR(f) CreateFileW(f, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)
 
-int __findlib(LPCSTR fname, LPSTR fpath, DWORD size);
-int __impl_static_ldd(struct LDD_ARGS args, LPCVOID fdat, LPCTSTR fname, int depth);
-int __impl_ldd(struct LDD_ARGS args, LPCTSTR fname, int depth);
+int __findlib(LPCWSTR fname, LPWSTR fpath, DWORD size);
+int __impl_static_ldd(struct LDD_ARGS args, LPCVOID fdat, LPCWSTR fname, int depth);
+int __impl_ldd(struct LDD_ARGS args, LPCWSTR fname, int depth);
 DWORD __rva2offset(DWORD rva, PIMAGE_SECTION_HEADER secH, PIMAGE_NT_HEADERS ntH);
 
-int ldd(const struct LDD_ARGS args, const LPCTSTR fname)
+int ldd(const struct LDD_ARGS args, const LPCWSTR fname)
 {
     return __impl_ldd(args, fname, 0);
 }
 
-int __impl_ldd(const struct LDD_ARGS args, const LPCTSTR fname, int depth)
+int __impl_ldd(const struct LDD_ARGS args, const LPCWSTR fname, int depth)
 {
-    TCHAR fpath[MAX_PATH];
-    HANDLE fp;
-    HANDLE fmap;
-    HANDLE fbase;
-    int ret = 0;
-
-    if (GetFullPathName(fname, MAX_PATH, fpath, NULL) == 0)
+    LPWSTR fpath = malloc(MAX_PATH_W * sizeof(WCHAR));
+    fpath[0] = 0;
+    if (GetFullPathNameW(fname, MAX_PATH_W, fpath, NULL) == 0)
     {
         _ctperr(fname, _T("Failed to get full path"));
         return -1;
     }
 
-    ret = 0;
-    fp = OpenFileR(fpath);
-    fmap = CreateFileMapping(fp, NULL, PAGE_READONLY, 0, 0, NULL);
+    int ret = 0;
+    HANDLE fp = OpenFileR(fpath);
+    free(fpath);
+    if (fp == NULL)
+    {
+        _ctperr(fname, _T("Failed to open file"));
+        _ctperrle(fname);
+        ret = -1;
+        goto FINALIZE_FP;
+    }
+    HANDLE fmap = CreateFileMappingW(fp, NULL, PAGE_READONLY, 0, 0, NULL);
     if (fmap == NULL)
     {
         _ctperr(fname, _T("Failed to create file mapping"));
         _ctperrle(fname);
         ret = -1;
-        goto FINALIZE;
+        goto FINALIZE_FMAP;
     }
-    fbase = MapViewOfFile(fmap, FILE_MAP_READ, 0, 0, 0);
+    HANDLE fbase = MapViewOfFile(fmap, FILE_MAP_READ, 0, 0, 0);
     if (fbase == NULL)
     {
         _ctperr(fname, _T("Failed to view file mapping"));
         _ctperrle(fname);
         ret = -1;
-        goto FINALIZE;
+        goto FINALIZE_FBASE;
     }
 
     ret = __impl_static_ldd(args, fbase, fname, depth);
 
-FINALIZE:
-    UnmapViewOfFile(fbase);
-    CloseHandle(fmap);
-    CloseHandle(fp);
+FINALIZE_FBASE:
+    if (fbase)
+        UnmapViewOfFile(fbase);
+FINALIZE_FMAP:
+    if (fmap)
+        CloseHandle(fmap);
+FINALIZE_FP:
+    if (fp)
+        CloseHandle(fp);
     return ret;
 }
 
-int __impl_static_ldd(struct LDD_ARGS args, const LPCVOID fdat, const LPCTSTR fname, int depth)
+int __impl_static_ldd(struct LDD_ARGS args, const LPCVOID fdat, const LPCWSTR fname, int depth)
 {
     PIMAGE_DOS_HEADER dosH = (PIMAGE_DOS_HEADER)fdat;
     PIMAGE_NT_HEADERS ntH = (PIMAGE_NT_HEADERS)((ULONG_PTR)fdat + dosH->e_lfanew);
@@ -62,87 +71,162 @@ int __impl_static_ldd(struct LDD_ARGS args, const LPCVOID fdat, const LPCTSTR fn
     DWORD impRVA;
     PIMAGE_IMPORT_DESCRIPTOR impDesc;
 
+    // filter non-DOS binary
     if (dosH->e_magic != IMAGE_DOS_SIGNATURE)
     {
-        _ctperr(fname, _T("DOS signature (MZ) not matched"));
+        if (depth == 0)
+            cwperr(fname, _T("DOS signature (MZ) not matched"));
         return -1;
     }
+    // filter non-NT binary
     if (ntH->Signature != IMAGE_NT_SIGNATURE)
     {
-        _ctperr(fname, _T("PE signature (PE) not matched"));
+        if (depth == 0)
+            cwperr(fname, _T("PE signature (PE) not matched"));
         return -1;
     }
+
+    // filter independent PE file
     if (ntH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].Size == 0)
     {
-        // _ctperr(fname, _T("Import table not detected"));
+        if (depth == 0)
+            cwperr(fname, _T("Dependency not found"));
         return -1;
     }
 
     impRVA = ntH->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
     impDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD_PTR)fdat + __rva2offset(impRVA, secH, ntH));
-    while (impDesc->Name != 0)
+    for (; impDesc->Name != 0; ++impDesc)
     {
-        LPCSTR name = (LPCSTR)((ULONG_PTR)fdat + __rva2offset(impDesc->Name, secH, ntH));
-        if ((strncmp("ext-", name, 4) == 0 || strncmp("api-", name, 4) == 0) && !args.bViewAll)
+        // convert name from UTF-8 to UTF-16
+        WCHAR namew[MAX_PATH_W];
+        namew[0] = 0;
         {
-            goto CONTINUE; // do not call continue. it causes infinite loops
+            LPCSTR namea = (LPCSTR)((ULONG_PTR)fdat + __rva2offset(impDesc->Name, secH, ntH));
+
+            // determine if library is Windows API sets
+            if ((strncmp("ext-", namea, 4) == 0 || strncmp("api-", namea, 4) == 0) && !args.bViewAll)
+                continue;
+
+            MultiByteToWideChar(CP_UTF8, 0, namea, strlen(namea) + 1, namew, MAX_PATH_W);
         }
 
-        CHAR fpath[MAX_PATH_A];
-        for (int i = 0; i < depth; ++i)
-            fputs("  ", stdout);
-        if (__findlib(name, fpath, MAX_PATH_A) != 0)
+        // print indents
+        for (int i = 0; i < depth + 4; ++i)
+            pmsg("  ");
+
+        // print library name
+        pmsgw(namew);
+        pmsg("  =>  ");
+
+        // print library path
+        LPWSTR fpath = malloc(MAX_PATH_W * sizeof(WCHAR));
+        fpath[0] = 0;
+
+        int found = __findlib(namew, fpath, MAX_PATH_W);
+        pmsgw((found == 0) ? fpath : L"not found");
+
+        // print library address
+        pmsg(" (");
+        HMODULE module = GetModuleHandleW(namew);
+        if (module != NULL)
         {
-            HMODULE module = GetModuleHandleA(name);
-            if (module != NULL)
-            {
-                printf("        %s  =>  not found (0x%08X)\n", name, fpath, module);
-            }
-            else
-            {
-                printf("        %s  =>  not found (not loaded)\n", name, fpath);
-            }
+            pmsg("0x");
+            pld64x((size_t)module);
         }
         else
         {
-            HMODULE module = GetModuleHandleA(name);
-            if (module != NULL)
-            {
-                printf("        %s  =>  %s (0x%08X)\n", name, fpath, module);
-            }
-            else
-            {
-                printf("        %s  =>  %s (not loaded)\n", name, fpath);
-            }
-            TCHAR tfpath[MAX_PATH];
-            MultiByteToWideChar(CP_UTF8, 0, fpath, MAX_PATH_A, tfpath, MAX_PATH);
-            __impl_ldd(args, tfpath, depth + 1);
+            pmsg("not loaded");
+        }
+        pmsg(")\n");
+
+        // recurse listing
+        if (found == 0)
+        {
+            __impl_ldd(args, fpath, depth + 1);
         }
 
-    CONTINUE:
-        impDesc++;
+        free(fpath);
     }
 
     return 0;
 }
 
-int __findlib(const LPCSTR fname, const LPSTR fpath, DWORD size)
+int __findlib(const LPCWSTR fname, const LPWSTR fpath, DWORD size)
 {
-    HMODULE lib = LoadLibraryExA(fname, NULL, LOAD_LIBRARY_AS_IMAGE_RESOURCE);
+    HMODULE lib = LoadLibraryExW(fname, NULL, LOAD_LIBRARY_AS_IMAGE_RESOURCE);
     if (lib == NULL)
     {
-        // cperr(fname, "Failed to load module");
-        // cperrle(fname);
+        cwperr(fname, L"Failed to load module");
+        cwperrle(fname);
         return -1;
     }
-    DWORD ret = GetModuleFileNameA(lib, fpath, size);
-    FreeLibrary(lib);
+
+    LPWSTR buf = malloc(MAX_PATH_W * sizeof(WCHAR));
+    buf[0] = 0;
+    DWORD ret = GetMappedFileNameW(GetCurrentProcess(), lib, buf, MAX_PATH_W);
     if (ret == 0)
     {
-        // cperr(fname, "Failed to get module file name");
-        // cperrle(fname);
+        cwperr(fname, L"Failed to get module file name");
+        cwperrle(fname);
+        FreeLibrary(lib);
+        free(buf);
         return -1;
     }
+
+    LPWSTR tmp = malloc(512 * sizeof(WCHAR));
+    tmp[0] = 0;
+    if (GetLogicalDriveStringsW(511, tmp) == 0)
+    {
+        cwperr(fname, L"Failed to get logical drive name");
+        cwperrle(fname);
+        FreeLibrary(lib);
+        free(buf);
+        free(tmp);
+        return -1;
+    }
+
+    WCHAR driv[3];
+    PWCHAR p = tmp;
+    BOOL found = 0;
+
+    driv[0] = L' ';
+    driv[1] = L':';
+    driv[2] = 0;
+
+    do
+    {
+        WCHAR name[MAX_PATH_W];
+        name[0] = 0;
+
+        driv[0] = p[0];
+        if (!QueryDosDeviceW(driv, name, MAX_PATH_W))
+        {
+            continue;
+        }
+
+        size_t namelen = wcslen(name);
+        if (namelen < MAX_PATH_W)
+        {
+            found = wcsnicmp(buf, name, namelen) == 0 && *(buf + namelen) == L'\\';
+            if (found)
+            {
+                WCHAR tmpfile[MAX_PATH_W];
+                tmpfile[0] = 0;
+                wcscat_s(tmpfile, MAX_PATH_W, driv);
+                wcscat_s(tmpfile, MAX_PATH_W, (PWCHAR)(buf + namelen));
+                wcscpy_s(buf, MAX_PATH_W, tmpfile);
+            }
+        }
+
+        while (*p++)
+            ;
+    } while (!found && *p);
+
+    wcscpy_s(fpath, size, buf);
+    FreeLibrary(lib);
+    free(buf);
+    free(tmp);
     return 0;
 }
 
