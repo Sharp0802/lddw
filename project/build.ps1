099c2c806a62541051a9c4f9d9e01a5ed3dfa7c6
Sharp0802 <sharp0802.business@gmail.com>
2023-02-22 05:34:03 +0900

    Change toolchain from CMake to Makefile
    - build.ps1: Move automation script to project folder
    - project/build/CMakeLists.txt: Remove file to use raw Makefile
    - project/Makefile: Create Makefile
    - project/build.ps1: Change automation script to use raw Makefile

--- project/build.ps1
+++ project/build.ps1
@@ -1,7 +1,17 @@
-set-location build
-$pre_path = $env:PATH
-$env:PATH = ($env:PATH.split(';') | where-object { $_ -like "*msys*" }) + ($env:PATH) -join ';'
-cmake.exe CMakeLists.txt -G "MinGW Makefiles" -DCMAKE_C_COMPILER="$($args[0])" -DCMAKE_CXX_COMPILER="$($args[0])"
-mingw32-make.exe
-$env:PATH = $pre_path
-set-location ..
+param(
+    [string]$Architecture,
+    [string]$Mode
+)
+
+$Path = Get-Location
+Set-Location $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+if (($Mode -ne "Debug") -and ($Mode -ne "Release")) { 
+    Write-Host "E: Unknown build mode" 
+    Exit 1
+}
+
+mingw32-make.exe clean
+mingw32-make.exe all MODE=$Mode ARCH=$Architecture
+
+Set-Location $Path
