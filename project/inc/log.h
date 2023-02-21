f77b21aa8cb3b00cfc310cf00ce61a1f44f98183
Sharp0802 <sharp0802.business@gmail.com>
2023-02-22 05:45:16 +0900

    Define new IO functions to replace direct call to IO functions
    - project/inc/log.h: Declare new IO functions to replace direct call to `fputs` or `fputws` etc
    - project/src/log.c: Define new IO functions to replace direct call to `fputs` or `fputws` etc

--- project/inc/log.h
+++ project/inc/log.h
@@ -5,6 +5,11 @@
 
 #define APP_NAME "lddw"
 
+void reqbr(void);
+
+void pmsg(LPCSTR msg);
+void pmsgw(LPCWSTR msg);
+
 void cperr(LPCSTR fname, LPCSTR msg);
 void cperre(LPCSTR fname, DWORD err);
 void cperrle(LPCSTR fname);
@@ -17,10 +22,21 @@ void cwperrle(LPCWSTR fname);
 #define _ctperr cwperr
 #define _ctperre cwperre
 #define _ctperrle cwperrle
+#define _pmsgt pmsgw
 #else
 #define _ctperr cperr
 #define _ctperre cperre
 #define _ctperrle cperrle
+#define _pmsgt pmsg
+#endif
+
+void pu32x(unsigned int u32);
+void pu64x(unsigned long long u64);
+
+#ifdef _WIN64
+#define pld64x pu64x
+#else
+#define pld64x pu32x
 #endif
 
 #endif
--- project/src/log.c
+++ project/src/log.c
@@ -2,22 +2,45 @@
 
 #define _L(t) L##t
 #define L(t) _L(t)
-#define FORMAT_FLAGS (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_ALLOCATE_BUFFER)
+#define FORMAT_FLAGS (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER)
+
+static bool linebreaked = 0;
+
+void reqbr(void)
+{
+    if (linebreaked)
+        return;
+    fputc('\n', stdout);
+    linebreaked = true;
+}
+
+void pmsg(LPCSTR msg)
+{
+    linebreaked = msg[strlen(msg) - 1] == '\n';
+    fputs(msg, stdout);
+}
+
+void pmsgw(LPCWSTR msg)
+{
+    linebreaked = msg[wcslen(msg) - 1] == L'\n';
+    fputws(msg, stdout);
+}
 
 #define __typ_str LPCSTR
 #define __loc_fmt FormatMessageA
-#define __loc_puts(m) fputs(m, stderr)
-#define __loc_prtx(i) fprintf(stderr, "0x%08lX", i)
+#define __loc_puts(m) pmsg(m)
+#define __loc_prtx(i) pu32x(i)
 #define __loc_prte __impl_cperre
 #define __exp_prte cperre
 #define __str(t) t
 
 void __impl_cperre(const __typ_str fname, const __typ_str msg, DWORD err)
 {
+    reqbr();
     __loc_puts(__str(APP_NAME));
     __loc_puts(__str(": "));
     __loc_puts(fname);
-    __loc_puts(__str(": "));
+    __loc_puts(__str(": 0x"));
     __loc_prtx(err);
     __loc_puts(__str(": "));
     __loc_puts(msg);
@@ -27,6 +50,7 @@ void __impl_cperre(const __typ_str fname, const __typ_str msg, DWORD err)
 
 void cperr(const __typ_str fname, const __typ_str msg)
 {
+    reqbr();
     __loc_puts(__str(APP_NAME));
     __loc_puts(__str(": "));
     __loc_puts(fname);
@@ -66,18 +90,19 @@ void cperrle(const __typ_str fname)
 
 #define __typ_str LPCWSTR
 #define __loc_fmt FormatMessageW
-#define __loc_puts(m) fputws(m, stderr)
-#define __loc_prtx(i) fwprintf(stderr, L"0x%08lX", i)
+#define __loc_puts(m) pmsgw(m)
+#define __loc_prtx(i) pu32x(i)
 #define __loc_prte __impl_cwperre
 #define __exp_prte cwperre
 #define __str(t) L(t)
 
 void __impl_cwperre(const __typ_str fname, const __typ_str msg, DWORD err)
 {
+    reqbr();
     __loc_puts(__str(APP_NAME));
     __loc_puts(__str(": "));
     __loc_puts(fname);
-    __loc_puts(__str(": "));
+    __loc_puts(__str(": 0x"));
     __loc_prtx(err);
     __loc_puts(__str(": "));
     __loc_puts(msg);
@@ -87,6 +112,7 @@ void __impl_cwperre(const __typ_str fname, const __typ_str msg, DWORD err)
 
 void cwperr(const __typ_str fname, const __typ_str msg)
 {
+    reqbr();
     __loc_puts(__str(APP_NAME));
     __loc_puts(__str(": "));
     __loc_puts(fname);
@@ -123,3 +149,29 @@ void cwperrle(const __typ_str fname)
 #undef __loc_prte
 #undef __exp_prte
 #undef __str
+
+void pu32x(unsigned int u32)
+{
+    const char hexdec[] = "0123456789ABCDEF";
+    char buf[5];
+    buf[4] = 0;
+    for (int i = 0; i < 4; ++i)
+    {
+        buf[3 - i] = hexdec[u32 % 16];
+        u32 /= 16;
+    }
+    pmsg(buf);
+}
+
+void pu64x(unsigned long long u64)
+{
+    const char hexdec[] = "0123456789ABCDEF";
+    char buf[9];
+    buf[8] = 0;
+    for (int i = 0; i < 8; ++i)
+    {
+        buf[7 - i] = hexdec[u64 % 16];
+        u64 /= 16;
+    }
+    pmsg(buf);
+}
